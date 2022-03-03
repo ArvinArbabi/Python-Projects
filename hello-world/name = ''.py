25e9034d63c2a619ff53fb24bb19c58c2ada82d6
Arvin Arbabi <arvin.arbabi50@gmail.com>
2022-03-02 19:59:34 -0800

    adding variables and defining dictionary and list

---
+++ hello-world/name = ''.py
@@ -0,0 +1,3 @@
+numbers = [5, 7, 8, 12]
+astring = "run time"
+astring[0] = "f"
---
+++ variables/variables_start.py
@@ -0,0 +1,90 @@
+# myint = 3
+
+# myint = "abc"
+
+# print(myint)
+
+# mylist = [0, 3, 4, 6, 8 , 3, 14, "hi", 67.4]
+# print(mylist[2])
+# mylist[2] = "Arvin"
+# print(mylist[2])
+
+# mytuple = (2, 4, 6, 8)
+# print(mytuple[1])
+
+# print(mylist[0:7:2])
+
+from uuid import NAMESPACE_X500
+
+
+us_state_to_abbrev = {
+    "Alabama": "AL",
+    "Alaska": "AK",
+    "Arizona": "AZ",
+    "Arkansas": "AR",
+    "California": "CA",
+    "Colorado": "CO",
+    "Connecticut": "CT",
+    "Delaware": "DE",
+    "Florida": "FL",
+    "Georgia": "GA",
+    "Hawaii": "HI",
+    "Idaho": "ID",
+    "Illinois": "IL",
+    "Indiana": "IN",
+    "Iowa": "IA",
+    "Kansas": "KS",
+    "Kentucky": "KY",
+    "Louisiana": "LA",
+    "Maine": "ME",
+    "Maryland": "MD",
+    "Massachusetts": "MA",
+    "Michigan": "MI",
+    "Minnesota": "MN",
+    "Mississippi": "MS",
+    "Missouri": "MO",
+    "Montana": "MT",
+    "Nebraska": "NE",
+    "Nevada": "NV",
+    "New Hampshire": "NH",
+    "New Jersey": "NJ",
+    "New Mexico": "NM",
+    "New York": "NY",
+    "North Carolina": "NC",
+    "North Dakota": "ND",
+    "Ohio": "OH",
+    "Oklahoma": "OK",
+    "Oregon": "OR",
+    "Pennsylvania": "PA",
+    "Rhode Island": "RI",
+    "South Carolina": "SC",
+    "South Dakota": "SD",
+    "Tennessee": "TN",
+    "Texas": "TX",
+    "Utah": "UT",
+    "Vermont": "VT",
+    "Virginia": "VA",
+    "Washington": "WA",
+    "West Virginia": "WV",
+    "Wisconsin": "WI",
+    "Wyoming": "WY",
+    "District of Columbia": "DC",
+    "American Samoa": "AS",
+    "Guam": "GU",
+    "Northern Mariana Islands": "MP",
+    "Puerto Rico": "PR",
+    "United States Minor Outlying Islands": "UM",
+    "U.S. Virgin Islands": "VI",
+}
+    
+# invert the dictionary
+abbrev_to_us_state = dict(map(reversed, us_state_to_abbrev.items()))
+
+print(us_state_to_abbrev["Arkansas"])
+
+def someFun():  
+    name = "artvin"
+    print(name)
+someFun()
+
+# print(name) because the variable is out of scope
