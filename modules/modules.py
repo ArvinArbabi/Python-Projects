f09918d1f43c42346395d94dc6037760b870b960
Arvin Arbabi <arvin.arbabi50@gmail.com>
2022-03-12 15:46:25 -0800

    learning about modules

---
+++ modules/modules.py
@@ -0,0 +1,14 @@
+from cmath import pi
+import math, random
+
+print("the square root of 16 is", math.sqrt(16))
+
+print("pi is", math.pi)
+
+print("sine of pi is", math.sin(math.pi))
+print(math.ceil(9.34))
+print(math.floor(9.34))
+print(round(9.5))
+print(math.log(100,math.e))
+print(math.fabs(-666.6))
+
